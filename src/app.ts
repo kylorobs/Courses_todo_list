33e480384a3983dce391430c02af2436f400cf8c
KCLSU <45687039+KCLSU@users.noreply.github.com>
2020-08-09 18:42:58 +0200

    create classes for each template + add decorators

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,67 @@
 // Code goes here!
 
-let single:SingleCourse;
+enum courseEnum {Active =' active', Finished = 'finished'}
+type courseType = courseEnum.Active | courseEnum.Finished;
+
+
+interface Course {
+    title: string,
+    description: string;
+    people: number;
+    id: string;
+    type: courseType;
+}
+
+
+
+class Course implements Course {
+    title: string;
+    description: string;
+    people: number;
+    id: string;
+
+    constructor(title: string, description: string, people: number){
+        this.title = title;
+        this.description = description;
+        this.people = people;
+        this.id = Math.random().toString(36).substr(2, 10);
+        this.type = courseEnum.Active;
+    }
+}
+
+class CourseState{
+
+    courses: Course[];
+    private listeners: Function[];
+    private static instance: CourseState;
+    
+    private constructor(){
+        this.courses =[];
+        this.listeners = [];
+    }
+
+    addCourse(course:Course){
+        this.courses.push(course);
+        
+        for (const listener of this.listeners){
+            listener([...this.courses])
+        }
+    }
+
+    addListener(listener: Function){
+        this.listeners.push(listener)
+    }
+
+    static createInstance(){
+        if (!this.instance){
+            return new CourseState();
+        }
+        else return this.instance;
+    }
+
+}
+
+const state = CourseState.createInstance();
 
 function BindThis( _: any, _1 : string, descriptor: PropertyDescriptor){
     const originalMethod = descriptor.value;
@@ -13,17 +74,7 @@ function BindThis( _: any, _1 : string, descriptor: PropertyDescriptor){
     return newDescriptor;
 }
 
-const metaValidations = {}
-
-function validate(){
-
-}
-
-
-
-
-
-class Project{
+class CourseForm {
 
     private template: HTMLTemplateElement;
     private host: HTMLDivElement;
@@ -56,12 +107,12 @@ class Project{
         this.formSubmit.addEventListener('click', this.newCourse)
     }
 
-    initialisePage(){
+    private initialisePage(){
         const content = this.template.content.cloneNode(true);
-        this.host.appendChild(content)
+        this.host.appendChild(content);
     }
 
-    validate(){
+    private validate(){
         let valid = true;
         if (!valid || this.formTitle.value.length <= 0) valid = false;
         else if (!valid ||  !this.formDescription || this.formDescription.value.length <=0) valid = false;
@@ -69,37 +120,78 @@ class Project{
         return valid;
     }
 
+    private clearInputs(){
+        this.formTitle.value = "";
+        this.formDescription.value = "";
+        this.formPeople.value = "";
+    }
+
     @BindThis
-    newCourse(e: Event){
+    private newCourse(e: Event){
         e.preventDefault();
         if (!this.validate()) alert('Invalid Input')
         else {
             const course = new Course(this.formTitle.value, this.formDescription.value, +this.formPeople.value);
-            single = SingleCourse.createInstance(this.formTitle.value)
-            this.courses.push(course)
+            state.addCourse(course);
+            this.clearInputs();
         }
     }
+}
+
+abstract class Component <T extends HTMLElement, U extends HTMLElement> {
+    private template: HTMLTemplateElement;
+    private parentElement: T;
+    private host: U
+
+    constructor(templateId: string, hostId: string, beforeEnd:boolean, assignId?:string){
+        this.template = document.getElementById(templateId)! as HTMLTemplateElement;
+        const imported =  document.importNode(this.template.content, true);
+        this.parentElement = imported.firstElementChild! as T;
+        this.host = document.getElementById(hostId)! as U;
+
+        if (assignId){
+            this.parentElement.id = assignId;
+        }
+
+        this.host.insertAdjacentElement(beforeEnd ? 'beforeend' : 'afterbegin', this.parentElement);
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+
 }
 
 
 class SingleCourse{
     private _name: string; 
-    // private template: HTMLTemplateElement;
     private host: HTMLDivElement;
     private static instance: SingleCourse;
+    private ulElement: HTMLUListElement;
 
     private constructor(name: string){
         console.log('running constructor')
         this._name = name;
-        // this.template = document.getElementById('single-project')! as HTMLTemplateElement
         this.host = document.getElementById('app')! as HTMLDivElement;
-        // this.singleItem = this.template.firstElementChild! as HTMLLIElement;
-        this.setupDOM()
+        this.ulElement = document.createElement('ul')! as HTMLUListElement;
+        state.addListener(this.updateName)
+        this.setupDOM();
+    }
+
+    @BindThis
+    updateName(courses: Course[]){
+        console.log(courses)
+        console.log('Updating Name')
+        this.name = courses[courses.length -1].title;
+        this.renderName();
+    }
+
+    renderName(){
+        let li = this.ulElement.querySelector('li')! as HTMLLIElement;
+        li.innerText = this.name;
     }
 
     private set name(name: string){
         this._name = name;
-        this.setupDOM();
     }
 
     private get name(){
@@ -107,52 +199,82 @@ class SingleCourse{
     }
 
     private setupDOM(){
-        // const content = this.template.content.cloneNode(true) as DocumentFragment;
-        let li: HTMLLIElement = this.host.querySelector('li')! as HTMLLIElement;
-        if (li) li.innerText = this.name;
-        else {
-            const ul:HTMLUListElement = document.createElement('ul')! as HTMLUListElement;
-            this.host.insertAdjacentElement('beforeend', ul);
-            console.log('setting up')
-            li = document.createElement('li')! as HTMLLIElement;
-            li.innerText = this.name;
-            ul.appendChild(li);
-        }
-
+        this.host.insertAdjacentElement('beforeend', this.ulElement);
+        console.log('setting up')
+        const li = document.createElement('li')! as HTMLLIElement;
+        this.ulElement.appendChild(li);
+        this.ulElement.id="single-course";
+        this.renderName();
     }
 
-    static createInstance(name: string) : SingleCourse{
-        console.log('running')
-        if (this.instance) {
-            console.log('Already an instance')
-            this.instance.name = name;
-            return this.instance
-        }
-        else {
-            console.log('no instance')
-            return this.instance = new SingleCourse(name);
-        }
+    static createInstance() : SingleCourse {
+        console.log('New Single Course Created')
+        if (this.instance) return this.instance;
+        else return this.instance = new SingleCourse(name);
     }
 }
 
 
-interface Course {
-    title: string,
-    description: string;
-    people: number;
-}
+class CourseList extends Component<HTMLElement, HTMLDivElement> {
 
-class Course implements Course {
-    title: string;
-    description: string;
-    people: number;
+    private assignedCourses:Course[];
+    private static instance: CourseList;
+    private type: courseType;
 
-    constructor(title: string, description: string, people: number){
-        this.title = title;
-        this.description = description;
-        this.people = people;
+    private constructor(type: courseType){
+        super("projects", "app", true, `${type}-projects`)
+        this.assignedCourses = state.courses;
+        this.type = type;
+        this.configure()
+        this.renderContent();
     }
+
+    static createInstance(type: courseType){
+        if (!this.instance){
+            return this.instance = new CourseList(type)
+        } else if (this.instance && this.instance.type !== type ){
+            return this.instance = new CourseList(type)
+        }
+        else return this.instance
+        
+    }  
+    
+    configure(){
+        state.addListener(this.updateCourses);
+    }
+
+    renderContent(){
+        const header = this.parentElement.querySelector('h2')!;
+        header.innerText = this.type;
+        this.renderCourses();
+    }
+
+    @BindThis
+    updateCourses(courses: Course[]){
+        this.assignedCourses = courses;
+        this.renderCourses()
+    }
+
+    private renderCourses(){
+        const ul = this.parentElement.querySelector('ul')! as HTMLUListElement;
+        const current = Array.from(this.parentElement.querySelectorAll('li'));
+        for (const course of this.assignedCourses){
+            if (course.type !== this.type) return;
+            else if (+current.findIndex(li => li.dataset.courseid === course.id) === -1){
+                let li = document.createElement('li');
+                li.innerText = course.title;
+                li.dataset.courseid = course.id;
+                ul.appendChild(li)
+            }
+            else console.log('Error')
+        }
+    }
+
 }
 
-const projectPage = new Project();
-// SingleCourse.createInstance('Michael')
+
+const projectPage = new CourseForm();
+const single = SingleCourse.createInstance();
+const active = CourseList.createInstance(courseEnum.Active);
+const finished = CourseList.createInstance(courseEnum.Finished);
+
